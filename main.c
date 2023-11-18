13ddd3c841dcb27b047aa24d1d80d0df61295106
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-18 22:21:30 +0000

    updated main.c file

--- main.c
+++ main.c
@@ -9,30 +9,42 @@
  */
 int main(int ac, char **av)
 {
-	char *line;
-	size_t len = 0;
+	stack_t *stack;
+	char *line_content;
+	size_t size = 0;
 	int line_number = 1;
-	ssize_t nread;
+	ssize_t nread = 1;
 	FILE *file;
 
 	/*check the number of arguments*/
 	if (ac != 2)
 	{
 		fprintf(stderr, "Usage: monty file\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	if (!isFile(av[1]))
 	{
 		fprintf(stderr, "Error: %s is not a valid file.\n", av[1]);
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	file = fopen(av[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: can't open file %s\n", av[1]);
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
+	}
+	while (nread > 0)
+	{
+		line_content = NULL;
+		nread = getline(&line_content, &size, file);
+		if (nread > 0)
+		{
+			exec_op(&stack, line_number, line_content, file);
+		}
+		line_number++;
+		free(line_content);
 	}
 	fclose(file);
-	free(line);
+	free_stack(stack);
 	return (0);
 }
