a79cf38685b08715f6f43f1d6733097d5cea17f6
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-18 22:20:46 +0000

    update on the exec func

--- utils.c
+++ utils.c
@@ -17,26 +17,42 @@ int isFile(const char *path)
  * exec_op - executes the operation
  * @stack: The stack to be manipulated
  * @linenumber: The line number of the operation
- * @op: the opcode
+ * @line_content: The content of the line
+ * @file: the file stream
+ * Return: 0 is successful else 1
  */
-void exec_op(stack_t **stack, unsigned int linenumber, char *op)
+int exec_op(stack_t **stack, unsigned int linenumber,
+		char *line_content, FILE *file)
 {
-	size_t i;
+	size_t i = 0;
+	char *op;
 	instruction_t instructions[] = {
-		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap},
-		{"add", add}, {"nop", nop}, {"sub", sub}, {"div", div}, {"mul", mul},
-		{"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, {"rotl", rotl},
-		{"rotr", rotr}, {NULL, NULL},
+		{"push", _push}, {"pall", _pall}, {"pint", _pint}, {"pop", _pop},
+		{"swap", _swap}, {"add", _add}, {"nop", _nop}, {"sub", _sub},
+		{"div", _div}, {"mul", _mul}, {"mod", _mod}, {"pchar", _pchar},
+		{"pstr", _pstr}, {"rotl", _rotl}, {"rotr", _rotr}, {NULL, NULL},
 	};
 
-	for (i = 0; i < (sizeof(instructions) / sizeof(instructions[0])); i++)
+	op = strtok(line_content, "\n\t");
+	if (op && op[0] == "#")
+		return (0);
+	stack_value = atoi(strtok(NULL, "\n\t"));
+	while (instructions[i].opcode && op)
 	{
 		if (strcmp(op, instructions[i].opcode) == 0)
 		{
 			instructions[i].f(stack, linenumber);
-			return;
+			return (0);
 		}
+		i++;
 	}
-	fprintf(stderr, "L%d: unknown instruction %s\n", linenumber, op);
-	exit(EXIT_FAILURE);
+	if (op && instructions[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", linenumber, op);
+		fclose(file);
+		free(line_content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
 }
