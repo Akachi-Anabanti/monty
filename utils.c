c5c038d9feb5350d3b210042761b86022dcef619
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-19 10:26:42 +0000

    checks if a str is an integer

--- utils.c
+++ utils.c
@@ -13,46 +13,24 @@ int isFile(const char *path)
 
 	return (S_ISREG(path_stat.st_mode));
 }
-/**
- * exec_op - executes the operation
- * @stack: The stack to be manipulated
- * @linenumber: The line number of the operation
- * @line_content: The content of the line
- * @file: the file stream
- * Return: 0 is successful else 1
- */
-int exec_op(stack_t **stack, unsigned int linenumber,
-		char *line_content, FILE *file)
-{
-	size_t i = 0;
-	char *op;
-	instruction_t instructions[] = {
-		{"push", _push}, {"pall", _pall}, {"pint", _pint}, {"pop", _pop},
-		{"swap", _swap}, {"add", _add}, {"nop", _nop}, {"sub", _sub},
-		{"div", _div}, {"mul", _mul}, {"mod", _mod}, {"pchar", _pchar},
-		{"pstr", _pstr}, {"rotl", _rotl}, {"rotr", _rotr}, {NULL, NULL},
-	};
 
-	op = strtok(line_content, "\n\t");
-	if (op && op[0] == "#")
-		return (0);
-	stack_value = atoi(strtok(NULL, "\n\t"));
-	while (instructions[i].opcode && op)
+/**
+ * isInteger - checks if a string is integer
+ * @str: string to check
+ * Return: true or false
+ */
+bool isInteger(char *str)
+{
+	int len = strlen(str);
+	int i = 0;
+
+	(str[i] =='-') ? i++ : i;
+
+	for (; i < len; i++)
 	{
-		if (strcmp(op, instructions[i].opcode) == 0)
-		{
-			instructions[i].f(stack, linenumber);
-			return (0);
-		}
-		i++;
+		if(!isdigit(str[i]))
+			return (false);
 	}
-	if (op && instructions[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", linenumber, op);
-		fclose(file);
-		free(line_content);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	return (1);
+	return (true);
 }
+
