5d5138d762d18b960dbc1704b9f6e91246f5460b
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-19 10:25:59 +0000

    prints all node values

---
+++ pall.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * _pall - prints all the values of the stack
+ * @head - pointer to head of the stack
+ * @line_number: The line number of the file
+ */
+void _pall(stack_t **head, unsigned int line_number)
+{
+	stack_t *temp;
+	(void)line_number;
+
+	if (!head)
+		return;
+
+	temp = *head;
+
+	while(temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
