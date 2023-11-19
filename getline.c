3a557ba77908d6bb4781672f80697b6bb2c23418
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-19 13:51:04 +0000

    custom getline function

--- getline.c
+++ getline.c
@@ -1,16 +1,48 @@
 #include "monty.h"
 /**
- * getline - getline function for c 89
- * @buffer: The text buffer
- * @size: the size of the string
- * @stream: The file stream
- * Return: buffer
+ * getline - getline function
+ * @lineptr: Pointer to the line content
+ * @n: size of the line content
+ * @stream: Input stream
+ * Return: number of character read
  */
-char *getline(char *buffer, int size, FILE *stream)
+ssize_t getline(char **lineptr, size_t *n, FILE *stream)
 {
-	if (fgets(buffer, size, stream) != NULL)
+	char *line = *lineptr, *new_line, c;
+	size_t i = 0, size;
+
+	if (line == NULL)
 	{
-		return (buffer);
+		if (*n)
+			size = *n;
+		else
+			size = 25;
+		line = malloc(size);
+		if (line == NULL)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			return (-1);
+		}
+		*n = size;
 	}
-	return (NULL);
+	while ((c = fgetc(stream)) != EOF)
+	{
+		if (i + 1 > *n)
+		{
+			new_line = _realloc(line, *n, *n * 2);
+			if (new_line ==  NULL)
+				return (-1);
+			line = new_line;
+			*n *= 2;
+		}
+
+		line[i++] = c;
+		if (c == '\n')
+			break;
+	}
+	if (i == 0 && c == EOF)
+		return (-1);
+	line[i] = '\0';
+	*lineptr = line;
+	return (i);
 }
--- utils.c
+++ utils.c
@@ -34,3 +34,34 @@ bool isInteger(char *str)
 	return (true);
 }
 
+/**
+ * _realloc - reallocates memory to a pointer
+ * @ptr: The pointer
+ * @new_size: New size of the pointer
+ * @old_size: Old size of the pointer
+ * Return: void memory pointer
+ */
+void *_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+	void *new_ptr;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
+	if (ptr != NULL)
+	{
+		if (old_size > new_size)
+			old_size = new_size;
+		memcpy(new_ptr, ptr, old_size);
+		free(ptr);
+	}
+	return (new_ptr);
+}
