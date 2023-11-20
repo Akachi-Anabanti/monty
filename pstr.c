1b3c799acd31bd8ecd4aa996b2d9f2b2fd9c35d5
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-20 17:28:25 +0000

    update pstr logic

--- pstr.c
+++ pstr.c
@@ -9,12 +9,8 @@ void _pstr(stack_t **head, unsigned int line_number)
 {
 	stack_t *aux;
 
+	(void)line_number;
 	aux = *head;
-	if (!aux)
-	{
-		fprintf(stderr, "L%d: can't pstr, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}
 	while (aux)
 	{
 		if (aux->n > 127 || aux->n <= 0)
