37e4f35d47fc6e702fea28c3f5511e246a799711
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-20 17:22:23 +0000

    update on loop check

--- pstr.c
+++ pstr.c
@@ -15,13 +15,10 @@ void _pstr(stack_t **head, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pstr, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (aux->n > 127 || aux->n < 0)
+	while (aux)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	while (aux->n != 0 && aux)
-	{
+		if (aux->n > 127 || aux->n <= 0)
+			break;
 		printf("%c", aux->n);
 		aux = aux->next;
 	}
