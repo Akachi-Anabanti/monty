fb07512f0cab6df24dfd2f392a1423698776214d
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-20 15:04:00 +0000

    adds the first two elements stores the value in the second element and frees the first element

--- exec.c
+++ exec.c
@@ -16,9 +16,8 @@ void exec_op(stack_t **stack, unsigned int linenumber,
 
 	instruction_t instructions[] = {
 		{"pall", _pall}, {"push", _push}, {"pint", _pint},
-		{"pop", _pop}, {"swap", _swap},
+		{"pop", _pop}, {"swap", _swap}, {"add", _add},
 		/**
-		 * {"add", _add},
 		 * {"nop", _nop},
 		 * {"sub", _sub},
 		 * {"div", _div}, {"mul", _mul}, {"mod", _mod}, {"pchar", _pchar},
--- monty.h
+++ monty.h
@@ -66,8 +66,9 @@ void _pall(stack_t **head, unsigned int line_number);
 void _pint(stack_t **head, unsigned int line_number);
 void _pop(stack_t **head, unsigned int line_number);
 void _swap(stack_t **head, unsigned int line_number);
-/*
 void _add(stack_t **head, unsigned int line_number);
+
+/*
 void _nop(stack_t **head, unsigned int line_number);
 void _sub(stack_t **head, unsigned int line_number);
 void _div(stack_t **head, unsigned int line_number);
